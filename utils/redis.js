18a4c65da98e72d1131622e1e8f5ee473e16b2b8
Amkeez252 <auwalamalik@gmail.com>
2023-09-03 12:40:44 +0000

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,63 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Display errors to the console
+    this.client.on('error', (err) => {
+      console.error(`Redis Error: ${err}`);
+    });
+  }
+
+  async isAlive() {
+    return new Promise((resolve) => {
+      this.client.ping('ping', (error, response) => {
+        if (error) {
+          resolve(false);
+        } else {
+          resolve(response === 'PONG');
+        }
+      });
+    });
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response === 1);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
