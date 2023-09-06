e8455d0b33829e8fdd2f82b343eed2ef02ac8f83
Amkeez252 <auwalamalik@gmail.com>
2023-09-06 21:11:38 +0000

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,41 +1,19 @@
-const AppController = {
-  getStatus: async (req, res) => {
-    try {
-      // Check Redis and DB status using your utility functions
-      // const redisStatus = await redisUtil.checkStatus();
-      // const dbStatus = await dbUtil.checkStatus();
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-      // For the sake of this example, we assume both Redis and DB are working
-      const redisStatus = true;
-      const dbStatus = true;
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
 
-      if (redisStatus && dbStatus) {
-        res.status(200).json({ "redis": true, "db": true });
-      } else {
-        res.status(500).json({ "error": "Redis or DB is not working" });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ "error": "Internal Server Error" });
-    }
-  },
-
-  getStats: async (req, res) => {
-    try {
-      // Use mongoose or any other database library to count users and files
-      // const userCount = await User.countDocuments();
-      // const fileCount = await File.countDocuments();
-
-      // For the sake of this example, we assume userCount and fileCount
-      const userCount = 4;
-      const fileCount = 30;
-
-      res.status(200).json({ "users": userCount, "files": fileCount });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ "error": "Internal Server Error" });
-    }
-  },
-};
-
-module.exports = AppController;
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// server.js
+import express from 'express';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const routes = require('./routes');
+const server = express();
 
-app.use(express.json());
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-// Use the routes defined in routes/index.js
-app.use('/', routes);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default server;
